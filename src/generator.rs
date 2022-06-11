ed01fb2a238d291d4683c2d614bd357049b713a3
Hiroaki Honda <82698421+Damin3927@users.noreply.github.com>
2022-06-11 09:52:03 +0900

    Change pwdgen to use clap

--- src/generator.rs
+++ src/generator.rs
@@ -1,41 +1,24 @@
+use crate::Cli;
 use rand::prelude::SliceRandom;
-use seahorse::Context;
 
-pub fn generate_password(ctx: &Context) {
+pub fn generate_password(cli: &Cli) -> String {
     let alphabet: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
     let number: Vec<char> = ('0'..='9').collect();
     let symbol: Vec<char> = r#"~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/"#.chars().collect();
 
-    if ctx.args.len() != 1 {
-        return println!(
-            "Error: Arg size is invalid. Expected {}, got {}",
-            1,
-            ctx.args.len()
-        );
-    }
-
-    let length_like = ctx.args[0].parse::<u8>();
-    if let Err(_) = length_like {
-        return println!(
-            "Error: Password legnth must be number. Given: {}",
-            ctx.args[0]
-        );
-    }
-    let length = length_like.unwrap();
-
     let mut source_letters: Vec<char> = Vec::new();
     source_letters.extend(alphabet);
     source_letters.extend(number);
-    if !ctx.bool_flag("exclude-symbols") {
+    if !cli.no_symbols {
         source_letters.extend(symbol);
     }
 
     let mut rng = rand::thread_rng();
     let mut password: Vec<char> = Vec::new();
-    for _ in 0..length {
+    for _ in 0..cli.length {
         let sample = *source_letters.choose(&mut rng).unwrap();
         password.push(sample);
     }
 
-    println!("{}", password.iter().collect::<String>());
+    password.iter().collect::<String>()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 mod generator;
 
+use clap::Parser;
 use generator::generate_password;
-use seahorse::{App, Flag, FlagType};
-use std::env;
+
+#[derive(Parser)]
+#[clap(
+    author = "Damin",
+    version = "0.1.0",
+    name = "pwdgen",
+    about = "Secure customizable password generator",
+    long_about = None
+)]
+pub struct Cli {
+    /// Password length
+    length: usize,
+
+    /// Exclude symbols from password
+    #[clap(short = 's', long)]
+    no_symbols: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let app = App::new("Password Generator")
-        .description("Secure customizable password generator")
-        .author("Damin")
-        .version("0.1.0")
-        .usage("pwdgen <length>")
-        .action(generate_password)
-        .flag(
-            Flag::new("exclude-symbols", FlagType::Bool)
-                .description("exclude symbols from password")
-                .alias("s"),
-        );
-
-    app.run(args);
+    let cli = Cli::parse();
+    let password = generate_password(&cli);
+    println!("{}", password);
 }
